3e48b005feb94f289973e8a8e8f15a3e116aeec7
romanb123 <44100462+romanb123@users.noreply.github.com>
2024-09-08 23:22:21 +0300

    Squashed commit of the following:
    commit e0c69657bd99fe5e1ef2bf52febe9225a3f3e75f
    Author: romanb123 <44100462+romanb123@users.noreply.github.com>
    Date:   Sun Sep 8 23:21:30 2024 +0300
    
        Update subscribe.css
    
    commit afe717b87137bb3517fb08fc0cb228201618e05b
    Author: romanb123 <44100462+romanb123@users.noreply.github.com>
    Date:   Sun Sep 8 23:17:51 2024 +0300
    
        code update
    
    commit d11bf934ecbf1dabd33d68cadf25d91ec062a151
    Author: romanb123 <44100462+romanb123@users.noreply.github.com>
    Date:   Sun Sep 8 23:10:39 2024 +0300
    
        code update
    
    commit 925592b9b1638a15ce077594715d88ef0fe9f56a
    Author: romanb123 <44100462+romanb123@users.noreply.github.com>
    Date:   Sun Sep 8 22:23:13 2024 +0300
    
        code update
    
    commit 4e821c8dfe072661a5ca28d3dcb0067924d3fb0f
    Author: romanb123 <44100462+romanb123@users.noreply.github.com>
    Date:   Sun Sep 8 21:56:44 2024 +0300
    
        code update
    
    commit 2dd7ea4f153a05ae6ee7a62f9af545fc6d202037
    Author: romanb123 <44100462+romanb123@users.noreply.github.com>
    Date:   Sun Sep 8 21:47:13 2024 +0300
    
        code update
    
    commit c50e06ddc6362ceb8c5ac057d04b4ae5afa6a669
    Author: romanb123 <44100462+romanb123@users.noreply.github.com>
    Date:   Sat Sep 7 23:57:38 2024 +0300
    
        code update
    
    commit 4f08766155fae31ba65866399dffb61f5d9f4e11
    Author: romanb123 <44100462+romanb123@users.noreply.github.com>
    Date:   Sat Sep 7 23:36:07 2024 +0300
    
        code update
    
    commit feb840dbcdc5936226709608c5b2da2d3eedb52a
    Author: romanb123 <44100462+romanb123@users.noreply.github.com>
    Date:   Sat Sep 7 23:16:01 2024 +0300
    
        code update
    
    commit a5a8b8100c8bbabd7c6732ca85af7777a806e13a
    Author: romanb123 <44100462+romanb123@users.noreply.github.com>
    Date:   Sat Sep 7 19:06:19 2024 +0300
    
        code update
    
    commit 7261dbc2d21488bd0bf9dbd0c26b20957ba4897c
    Author: romanb123 <44100462+romanb123@users.noreply.github.com>
    Date:   Sat Sep 7 18:50:30 2024 +0300
    
        Update slider.css
    
    commit f17f25e0c3b3c3203c49ff4b00b1277635f1abae
    Author: romanb123 <44100462+romanb123@users.noreply.github.com>
    Date:   Sat Sep 7 18:29:22 2024 +0300
    
        code updaete
    
    commit 95a48d64a0a5ad3a64d38be5d44082e46d6a1dd0
    Author: romanb123 <44100462+romanb123@users.noreply.github.com>
    Date:   Sat Sep 7 17:33:56 2024 +0300
    
        code update

---
+++ functions.js
@@ -0,0 +1,22 @@
+realEstate = {
+    init: function() {
+        realEstate.initSlider();
+    },
+
+    initSlider: function(){
+        $('.nonloop').owlCarousel({
+            center: true,
+            items:1.8,
+            loop:false,
+            margin:0,
+            responsive:{
+                600:{
+                    items:1.8
+                }
+            }
+        });
+    }
+}
+document.addEventListener("DOMContentLoaded", function() {
+    realEstate.init();
+}); 
